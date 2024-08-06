45bb430cbd5ea6373833a3b8d52368be28a6c700
Saurelien <aurelienschmitt.info@gmail.com>
2024-08-06 16:36:22 +0200

    Remove .idea from version control

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from .models import Letting, Address, Profile
 
-from .models import Letting
-from .models import Address
-from .models import Profile
 
+class MyAdminSite(AdminSite):
+    class Media:
+        css = {
+            'all': ('css/dark_theme.css',)
+        }
+
+
+admin.site = MyAdminSite()
 
 admin.site.register(Letting)
 admin.site.register(Address)
